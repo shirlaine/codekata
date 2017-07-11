705acc37d50659da435f77a8e8aa12fce5b8b1b5
shirlaine <shirlaine.phang@gmail.com>
2017-07-11 17:13:08 +0800

    Exercise 2

---
+++ 8-kyu/Add_item_to_an_Array.rb
@@ -0,0 +1,17 @@
+=begin
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+=end
+
+websites = []
+
+# add the value "codewars" to the already defined websites array
+
+websites.push("codewars")
+
+p websites
+
+
+
