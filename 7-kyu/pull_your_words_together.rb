e1f8ec40562606f895a1dbfae3ad004a7e65fdba
shirlaine <shirlaine.phang@gmail.com>
2017-09-12 22:27:14 +0800

    Add exercse

---
+++ 7-kyu/pull_your_words_together.rb
@@ -0,0 +1,42 @@
+=begin
+
+Your friend Robbie has successfully created an AI that is capable of communicating in English!
+
+Robbie's almost done with the project, however the machine's output isn't working as expected. Here's a sample of a sentence that it outputs:
+
+["this","is","a","sentence"]
+Every time that it tries to say a sentence, instead of formatting it in normal English orthography, it just outputs a list of words.
+
+Robbie has pulled multiple all-nighters to get this project finished, and he needs some beauty sleep. So, he wants you to write the last part of his code, a sentencify function, which takes the output that the machine gives, and formats it into proper English orthography.
+
+Your function should:
+
+Capitalise the first letter of the first word.
+Add a period (.) to the end of the sentence.
+Join the words into a complete string, with spaces.
+Do no other manipulation on the words.
+
+=end
+
+def sentencify(words)
+  MakeSentence.new(words).process
+end
+
+class MakeSentence
+
+  def initialize(word_array)
+    @word_array= word_array
+  end
+
+  def process
+    @word_array[0][0] = @word_array[0][0].capitalize
+    @word_array.join(" ").concat(".")
+  end
+
+end
+
+p sentencify(["i", "love", "apples"])
+p sentencify(["FIELDS","of","CORN","are","to","be","sown"])
+p sentencify(["yes"])
+
+
