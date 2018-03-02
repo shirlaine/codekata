040e6fd11b79e53b56e8e7c53e4a38f53b72ec48
shirlaine <shirlaine.phang@gmail.com>
2018-03-02 17:45:28 +0800

    Edit exercise

--- Javascript/general_algos/reverse-integers/reverse-integer-1.js
+++ Javascript/general_algos/reverse-integers/reverse-integer-1.js
@@ -7,23 +7,21 @@
 //reverseInt(-90) //-9
 
 const reverseInt = (num) => {
-
-    let numAbs = Math.abs(num)  //convert all to positive
-    let numArr = []
-    numAbs= numAbs.toString().split('') //convert number to string, split to arry
-    for (let char of numAbs) { //check for zeroes in array, remove if present
-      if (char!=='0') {
-      numArr=numArr.concat(char)
-      }
+  let numAbs = Math.abs(num)  //convert all to positive
+  let numArr = []
+  numAbs= numAbs.toString().split('') //convert number to string, split to arry
+  for (let char of numAbs) { //check for zeroes in array, remove if present
+    if (char!=='0') {
+    numArr=numArr.concat(char)
     }
-    numArr = parseInt(numArr.reverse().join('')) //reverse el in array, convert string, convert to numbers
+  }
+  numArr = parseInt(numArr.reverse().join('')) //reverse el in array, convert string, convert to numbers
 
-    if(num>=0) {  //check if initial number negative or positive and return accordinly
-      return numArr
-    } else {
-      return -numArr
-    }
-
+  if(num>=0) {  //check if initial number negative or positive and return accordinly
+    return numArr
+  } else {
+    return -numArr
+  }
 }
 
 console.log(reverseInt(15)) //51
